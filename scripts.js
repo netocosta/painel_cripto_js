5c062bdb0781f080eb0190b53cd043b2f142879e
Neto Costa <54679690+netocosta@users.noreply.github.com>
2022-10-19 22:27:49 -0300

    Correção de bug
    Ao remover todas as moedas, o valor geral não estava zerando.
    Alterado também a sequencia de perguntas ao efetuar a mudança.

--- scripts.js
+++ scripts.js
@@ -9,14 +9,14 @@ if (localStorage.getItem('data') === null) {
 
 _load() // Carrega o script
 
-var spots = [ // simbolo da moeda
-    { spot: 'brl', symbol: 'R$', },
-    { spot: 'usd', symbol: '$', }
-]
-
 function _load() {
+    var spots = [ // simbolo da moeda
+        { spot: 'brl', symbol: 'R$', },
+        { spot: 'usd', symbol: '$', }
+    ]
     var spot = document.getElementById("resposta").checked ? 'usd' : 'brl'
-
+    var symbol_spot = spots.filter((item) => item.spot.includes(spot))[0].symbol
+    
     // Limpa a tabela
     let tabela = document.getElementById("dados")
     tabela.innerHTML = ""
@@ -24,11 +24,12 @@ function _load() {
     // Carrega os dados do localStorage
     var data = JSON.parse(localStorage.getItem('data'))
 
-    let valorSaldo = 0
-    let saldo = document.getElementsByClassName('saldo')[0]
+    var valorSaldo = 0
+    var saldo = document.getElementsByClassName('saldo')[0]
     const formatNumber = new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
     const formatNumberCripto = new Intl.NumberFormat('en-US', { minimumFractionDigits: 8, maximumFractionDigits: 8 })
 
+    saldo.innerHTML = `${symbol_spot} ${formatNumber.format(valorSaldo)}`
     data.forEach(function (data, index) {
         consultar(data.api_id, spot).then(function (total) {
             const api = total[0]
@@ -39,11 +40,10 @@ function _load() {
             const current_price = api ? api.current_price : "{price}"
             const price_change_percentage_24h = api ? api.price_change_percentage_24h : "{percent}"
             const my_balance = data.balance
-            const symbol_spot = spots.filter((item) => item.spot.includes(spot))[0].symbol
             const variantcor = api.price_change_percentage_24h > 0 ? 'green' : 'red'
 
-            let tabela = document.getElementById("dados")
-            let anterior = tabela.innerHTML
+            var tabela = document.getElementById("dados")
+            var anterior = tabela.innerHTML
             tabela.innerHTML = [
                 anterior,
                 `<tr class="data">`,
@@ -61,7 +61,7 @@ function _load() {
             saldo.innerHTML = `${symbol_spot} ${formatNumber.format(valorSaldo)}`
         })
     });
-
+    
     async function consultar(moeda, cotacao_em) {
         try {
             const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?ids=${moeda}&vs_currency=${cotacao_em}`, {
@@ -75,6 +75,8 @@ function _load() {
         }
     }
 }
+
+
 
 function adicionar() {
     alert("Consulte em www.coingecko.com qual o API ID da moeda que deseja adicionar.")
@@ -106,19 +108,19 @@ function remover(index) {
 function editar(index) {
     data = JSON.parse(localStorage.getItem('data'))
 
-    // Alterar a carteira
-    if (confirm(`Deseja modificar a carteira onde está ${data[index].name} ?`)) {
-        new_wallet = prompt(`Informe a nova carteira para ${data[index].name}. (Ex: Binance)`)
-        montar = { wallet: new_wallet, api_id: data[index].api_id, symbol: data[index].symbol, name: data[index].name, balance: data[index].balance }
-        data.splice(index, 1)
-        data.push(montar)
-        localStorage.setItem('data', JSON.stringify(data));
-    }
-
     // Alterar o saldo
     if (confirm(`Deseja modificar o saldo de ${data[index].name} ?`)) {
         new_balance = prompt(`Informe o novo saldo para ${data[index].name}. (Ex: 0.12345678)`)
         montar = { wallet: data[index].wallet, api_id: data[index].api_id, symbol: data[index].symbol, name: data[index].name, balance: new_balance }
+        data.splice(index, 1)
+        data.push(montar)
+        localStorage.setItem('data', JSON.stringify(data));
+    }
+    
+    // Alterar a carteira
+    if (confirm(`Deseja modificar a carteira onde está ${data[index].name} ?`)) {
+        new_wallet = prompt(`Informe a nova carteira para ${data[index].name}. (Ex: Binance)`)
+        montar = { wallet: new_wallet, api_id: data[index].api_id, symbol: data[index].symbol, name: data[index].name, balance: data[index].balance }
         data.splice(index, 1)
         data.push(montar)
         localStorage.setItem('data', JSON.stringify(data));
